399d547faff4cf0bc5c40af93e314dc196c88195
FarDean <sfh1212@gmail.com>
2021-03-08 11:51:24 +0330

    projects done

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ burger.addEventListener("click", () => {
 		menu.style.height = "100vh";
 		Array.from(menu.children).forEach(li => {
 			li.style.opacity = 1;
+			li.style.display = "block";
 		});
 	} else {
 		menu.style.height = "0";
 		Array.from(menu.children).forEach(li => {
 			li.style.opacity = 0;
+			li.style.display = "none";
 		});
 	}
 });
